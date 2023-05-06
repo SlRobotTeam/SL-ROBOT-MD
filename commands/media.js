07c63ee7a7d33845421c9e69ce0f9a8a24c2565c
npserverr@gmail.com <npserverr@gmail.com>
2023-05-06 21:22:46 +0530

    resource

---
+++ commands/media.js
@@ -0,0 +1,46 @@
+const { fetchJson, cmd, citel, Config, tlang } = require('../lib')
+
+cmd({
+
+            pattern: "apk",
+
+            desc: "apk down",
+
+            react: "🗃️",
+
+            category: "downloader"
+
+        },
+
+        async(Void, citel, text) => {
+
+            if (!text) return
+
+const apkdl = await fetchJson(`https://darkalphaxteam-official-api.cyclic.app/api/download/apk?packagename=${text}&apikey=prabath`)
+            citel.reply (`*Hello ${citel.pushName} I Am Finding Your Apk*`);
+const applink = apkdl.data.dllink
+const getname = apkdl.data.name
+const lastupdate = apkdl.data.lastup
+
+       return Void.sendMessage(citel.chat, {
+
+                document: {
+
+                    url: applink ,
+
+                },
+
+                mimetype: "application/vnd.android.package-archive",
+                caption: tlang().footer,
+
+                fileName: getname ,
+
+            }, {
+
+                quoted: citel,
+
+            });
+
+        }
+
+    )
