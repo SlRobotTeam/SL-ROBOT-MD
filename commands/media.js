6ad85973445bb7d05a10c64e4534982d9843ce64
npserverr@gmail.com <npserverr@gmail.com>
2023-05-06 22:00:10 +0530

    resource

--- commands/media.js
+++ commands/media.js
@@ -1,4 +1,8 @@
+const axios = require('axios')
+const fs=require("fs")
 const { fetchJson, cmd, citel, Config, tlang } = require('../lib')
+
+//========================================================================================//
 
 cmd({
 
@@ -16,8 +20,8 @@ cmd({
 
             if (!text) return
 
-const apkdl = await fetchJson(`https://darkalphaxteam-official-api.cyclic.app/api/download/apk?packagename=${text}&apikey=prabath`)
-            citel.reply (`*Hello ${citel.pushName} I Am Finding Your Apk*`);
+apkdl = await axios.get(`https://darkalphaxteam-official-api.cyclic.app/api/download/apk?packagename=${text}&apikey=prabath`)
+citel.reply (`*Hello ${citel.pushName} I Am Finding Your Apk*`);
 const applink = apkdl.data.result.dllink
 const getname = apkdl.data.result.name
 const lastupdate = apkdl.data.result.lastup
@@ -44,3 +48,47 @@ const lastupdate = apkdl.data.result.lastup
         }
 
     )
+
+//========================================================================================//
+
+cmd({
+
+            pattern: "hirunews",
+
+            desc: "Hiru Tv News",
+
+            react: "️🗞️",
+
+            category: "search"
+
+        },
+
+        async(Void, citel, text) => {
+
+            if (!text) return
+
+hirunews = await axios.get(`https://api.sdbots.tk/hirunews`)
+
+       return Void.sendMessage(citel.chat, {
+
+                document: {
+
+                    url: hirunews.data.img ,
+
+                },
+
+                mimetype: "application/vnd.android.package-archive",
+                caption: "*ᴛɪᴛʟᴇ :* ${hirunews.data.title}\n\n*ᴅᴀᴛᴇ :* ${hirunews.data.date}\n\n*ʟɪɴᴋ :* ${hirunews.data.link}\n\n*ᴅᴇꜱᴄʀɪᴘᴛɪᴏɴ :* ${hirunews.data.description}\n\n*ᴅᴀʀᴋ ᴀɴɢᴇʟ ᴡʜᴀᴛꜱᴀᴘᴘ ʙᴏᴛ*",
+
+
+            }, {
+
+                quoted: citel,
+
+            });
+
+        }
+
+    )
+    
+//========================================================================================//
