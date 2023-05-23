ef43b4b8f207b5ff147d419a1c125d249c202042
SL-ROBOT®TEAM <121673113+SlRobotTeam@users.noreply.github.com>
2023-05-23 12:17:47 +0530

    Update Instagram.js

--- commands/Instagram.js
+++ commands/Instagram.js
@@ -6,9 +6,9 @@ cmd({
         filename: __filename
     },
     async(Void, citel,text,{isCreator}) => {
-if(!text) return citel.reply('Pasindu-MD Need post url.')
+if(!text) return citel.reply('🆂🅻 🆁🅱🅾🆃 🅼🅳 Need post url.')
 let response = await Insta(text)
 for (let i=0;i<response.length;i++) {
-await Void.sendFileUrl(citel.chat, response[i], `*Downloaded Media from instagram.*`, citel)
+await Void.sendFileUrl(citel.chat, response[i], `🅳🅾🆆🅽🅳🅾🅸🅽🅶 🅸🅽🆂🆃🅰...´, citel)
 }
     });
