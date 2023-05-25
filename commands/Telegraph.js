d764428989196b4418ad4fa3abd7d9e11103a831
SL-ROBOT®TEAM <121673113+SlRobotTeam@users.noreply.github.com>
2023-05-25 15:29:21 +0530

    Update Telegraph.js

--- commands/Telegraph.js
+++ commands/Telegraph.js
@@ -10,18 +10,10 @@
  * @version 0.0.6
  **/
 //---------------------------------------------------------------------------
-const { cmd }   = require('../lib');
-const util = require('util');
-const axios = require('axios');
-cmd({
-        pattern: "paste",
-        desc: "create paste of text.",
-        category: "general",
-        filename: __filename,
-    },
-    async(Void, citel,text) => {
- let a = citel.quoted ? citel.quoted.text : citel.text;
-let { data } = await axios.get(`https://api.telegra.ph/createPage?access_token=d3b25feccb89e508a9114afb82aa421fe2a9712b963b387cc5ad71e58722&title=Pasindu-Md+Bot&author_name=SamPandey001&content=[%7B"tag":"p","children":["${a.replace(/ /g,'+')}"]%7D]&return_content=true`);
-return citel.reply(`*Paste created on telegraph*\nName:-${util.format(data.result.title)} \nUrl:- ${util.format(data.result.url)}`)
-    }
-);
+const { Insta,cmd } = require('../lib')
+cmd({         pattern: "insta",         desc: "download instagram post.",         category: "downloader",         filename: __filename     },     async(Void, citel,text,{isCreator}) => {
+if(!text) return citel.reply('Pasindu-MD Need post url.')
+let response = await Insta(text)
+for (let i=0;i<response.length;i++) {
+await Void.sendFileUrl(citel.chat, response[i], `*Downloaded Media from instagram.*`, citel)
+}     }); 
